1a05bb313f2de4780a1acf1e01119357cb72fdc7
Toggle Corporation <jamie@programmer.net>
2020-08-01 16:04:09 +0100

    Created a readme file, and the RllyBadScam file
    Just an ordinary day :)

---
+++ RllyBadScam/index.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace NameGrab
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // User Name
+      string name = "Farhad Hesam Abbasi";
+
+      // Get first letter
+      int charPosition = name.IndexOf("F");
+      char firstLetter = name[charPosition];
+
+      // Get last name
+      int lastnamePosition = name.IndexOf("Hesam Abbasi");
+      string lastName = name.Substring(lastnamePosition);
+
+      // Print results
+      Console.WriteLine($"Dear {firstLetter}. {lastName}, you have one millon dollar!\r\nPlese clik on the link beloe!");
+
+    }
+  }
+}
